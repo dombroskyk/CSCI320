aba0bfad564c03174925163e785d4bb166a967d4
Antonite <axp9304@rit.edu>
2015-04-16 06:05:36 -0400

    Barebones of web application

---
+++ LoLCompare/main.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="main.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+    <title>hello title</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="search_box">
+        <asp:TextBox id="box" runat="server" />
+        <asp:Button ID="findSummoner" runat="server" OnClick="submit" Text="Find Summoner" />
+    </div>
+    <div id="summoner_info">
+        <asp:Label ID="summoner_id" runat="server" Text="Label"></asp:Label>
+        <br/>
+        <asp:Label ID="champion_id" runat="server" Text="Label"></asp:Label>
+        <br/>
+        <asp:Label ID="played_as_total" runat="server" Text="Label"></asp:Label>
+    </div>
+    <div id="server_status">
+         <asp:Label ID="status" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+
+</body>
+</html>
---
+++ LoLCompare/main.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using MySql.Data.MySqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        summoner_id.Text = "";
+        status.Text = "";
+        champion_id.Text = "";
+        played_as_total.Text = "";
+    }
+
+    protected void submit(object sender, EventArgs e)
+    {
+        status.Text = "Did not connect to the Database Server.";
+
+        //connect to server
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["lolmatchupsDB"].ConnectionString;
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        MySqlDataReader reader;
+
+        //search for player
+        MySqlCommand cmd = new MySqlCommand();
+        cmd.CommandText = "SELECT * FROM lolmatchups.player_champion_stat where summoner_id=" + box.Text;
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = connection;
+        connection.Open();
+        status.Text = "Connected to Database Server !!";
+
+        try
+        {
+            reader = cmd.ExecuteReader();
+            reader.Read();
+
+            //print data
+            summoner_id.Text = "Summoner: " + reader.GetInt16(0).ToString();
+            champion_id.Text = "Champion: " + reader.GetInt16(1).ToString();
+            played_as_total.Text = "Played as total: " + reader.GetInt16(3).ToString();
+            //other data..
+        }
+        catch (Exception conException)
+        {
+            summoner_id.Text = "summoner id not found";
+        }
+        connection.Close();
+
+        box.Text = "";
+    }
+
+}
